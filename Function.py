9a8ebc178d07e18710ae855361ccafae1342ca72
Weronika Piotrowska <piotrowska.w00@gmail.com>
2023-06-11 14:25:03 +0200

    removed function.py

--- Function.py
+++
@@ -1,8 +0,0 @@
-class Function(object):
-    def __init__(self, dimension: int, lower_bound: int | float, upper_bound: int | float):
-        self.dim = dimension
-        self.l_bound = lower_bound
-        self.u_bound = upper_bound
-
-    def evaluate(self, x):
-        raise NotImplementedError
